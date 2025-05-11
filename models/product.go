7c521474f0ab57301cb45768f09a42c7a0eff69e
AnceYin <570951613@qq.com>
2025-05-11 17:52:34 +0800

    buffalo generate resource Product name:string price:float 使用官方生成器生成crud代码

---
+++ models/product.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"encoding/json"
+	"time"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gobuffalo/validate/v3/validators"
+	"github.com/gofrs/uuid"
+)
+
+// Product is used by pop to map your products database table to your go code.
+type Product struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Price     float64   `json:"price" db:"price"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+// String is not required by pop and may be deleted
+func (p Product) String() string {
+	jp, _ := json.Marshal(p)
+	return string(jp)
+}
+
+// Products is not required by pop and may be deleted
+type Products []Product
+
+// String is not required by pop and may be deleted
+func (p Products) String() string {
+	jp, _ := json.Marshal(p)
+	return string(jp)
+}
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (p *Product) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.Validate(
+		&validators.StringIsPresent{Field: p.Name, Name: "Name"},
+	), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (p *Product) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (p *Product) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
